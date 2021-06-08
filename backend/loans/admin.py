a05296f5230a669c71652748d5cbab81735f7f14
devdattakhoche <dkhoche70@gmail.com>
2021-06-08 22:57:50 +0530

    User models added

--- backend/loans/admin.py
+++ backend/loans/admin.py
@@ -1,3 +1,6 @@
+from backend.loans.models import Loans, ProductMapping
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Loans)
+admin.site.register(ProductMapping)
